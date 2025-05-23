e5faf8997c320c1edb9b086e1971abdb69e49b85
EdklauseDiniz <edklausezinho@gmail.com>
2025-05-23 18:40:15 -0300

    Add files via upload

---
+++ Atividades Luciano/Atividades Luciano/Repeticao.py
@@ -0,0 +1,33 @@
+"""Questão 12"""
+
+def questao12():
+    idademenor = 999999999999999999999999999999999999
+    idademaior = 0
+    velho = ""
+    jovem = ""
+    while True:
+        idade = input("Digite a idade de uma pessoa: ")
+        idade = int(idade)
+        if idade == 0:
+            break
+        nome = input("Digite o nome dessa pessoa: ")
+        if idade < idademenor:
+            idademenor = idade
+            jovem = nome
+        if idade > idademaior:
+            idademaior = idade
+            velho = nome
+    print("A/s maior/es idade/s foi/foram de: "+velho)
+    print("A/s menor/es idade/s foi/foram de: "+jovem)
+
+"""Questão 13"""
+
+def questao13():
+    numero13 = 1
+    soma = 0
+    for numero13 in 50:
+        if (numero13%2) == 0:
+            soma = numero13+soma
+        print(soma)
+        
+questao13()
